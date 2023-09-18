616f650cef191ac8978566e85ec81b1d60ef597c
muhammad423 <mi1933065@gmail.com>
2023-09-18 01:56:14 -0700

    changes

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ export default function SignInPage() {
   return (
     <div className="flex items-center justify-center h-full mb-10 w-full">
 
-        <SignIn />;
+        <SignIn />
     </div>
         )
 }
--- src/components/ShowMore.tsx
+++ src/components/ShowMore.tsx
@@ -1,25 +1,28 @@
-'use client'
-import { useRouter } from 'next/navigation'
-import { ShowMoreProps } from '@/types/interfaces'
-import React from 'react'
-import { CustomBtn } from '.'
-import { updateSerchParams } from '@/utils/carsApi'
+"use client";
+import { useRouter } from "next/navigation";
+import { ShowMoreProps } from "@/types/interfaces";
+import React from "react";
+import { CustomBtn } from ".";
+import { updateSerchParams } from "@/utils/carsApi";
 
-const ShowMore = ({pageNumber, isNext}:ShowMoreProps) => {
-    const router = useRouter()
-    const handleNavigation = () => {
-        const newLimit = (pageNumber + 1) * 10
-        const newPathName = updateSerchParams('limit', `${newLimit}`)
-        router.push(newPathName)
-    }
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathName = updateSerchParams("limit", `${newLimit}`);
+    router.push(newPathName);
+  };
   return (
-    <div className=' flex items-center justify-center mt-10'>
-        {
-            !isNext && <CustomBtn  customCls='px-4 py-2  text-center bg-[#3E977C] hover:bg-[#2B7257] text-white text-base font-medium rounded-full' title='Show More' handleClick={handleNavigation}/>
-        }
-        
-        </div>
-  )
-}
+    <div className=" flex items-center justify-center mt-10">
+      {!isNext && (
+        <CustomBtn
+          customCls="px-4 py-2  text-center bg-[#3E977C] hover:bg-[#2B7257] text-white text-base font-medium rounded-full"
+          title="Show More"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
 
-export default ShowMore
+export default ShowMore;
